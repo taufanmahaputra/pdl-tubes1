f759714b62b27917bb7b6d66c0a100fe66ff567b
Taufan Mahaputra <hacker.indo62@gmail.com>
2019-02-21 16:49:34 +0700

    Add select function

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,18 @@ var { sequelizeClient } = require('../core/database')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  res.render('index', { title: 'PDL TUBES 1', results: [] });
+});
+
+router.post('/select', function(req, res, next) {
   sequelizeClient.query("SELECT * FROM room_reservations").spread((results, metadata) => {
-    res.json(results)
+    res.render('index', { title: 'PDL TUBES 1 | SELECT', results: results})
+  })
+});
+
+router.post('/insert', function(req, res, next) {
+  sequelizeClient.query("SELECT * FROM room_reservations").spread((results, metadata) => {
+    res.render('index', { title: 'PDL TUBES 1 | SELECT', results: results})
   })
 });
 
